be5eaf2d849ab279a417045bc656d67a800e7713
Samuel Gómez <samusmusic18@gmail.com>
2025-12-15 00:43:52 -0500

    Agregando la clase Persona como clase padre y para añadir polimorfismo

---
+++ Persona.h
@@ -0,0 +1,20 @@
+#ifndef PERSONA_H
+#define PERSONA_H
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Persona {
+protected:
+    int id;
+    string nombre;
+    string dni;
+public:
+    Persona();
+    Persona(int , string , string );
+    int getId();
+    string getNombre();
+    virtual void mostrarPersona();
+};
+
+#endif
