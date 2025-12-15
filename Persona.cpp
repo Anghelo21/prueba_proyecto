7633d60170a9f56b5ce66733198b11c8791cd73a
Samuel Gómez <samusmusic18@gmail.com>
2025-12-15 00:44:54 -0500

    Agregando las funciones de la clase Persona

---
+++ Persona.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include "Persona.h"
+using namespace std;
+
+Persona::Persona(){
+	id = 0;
+	nombre = " ";
+	dni = " ";
+}
+
+Persona::Persona(int i, string n, string d){
+	id = i;
+	nombre = n;
+	dni = d;
+}
+
+int Persona::getId(){
+	return id;
+}
+
+string Persona::getNombre(){
+	return nombre;
+}
+
+void Persona::mostrarPersona(){
+	cout<<"ID: "<<id<<endl;
+	cout<<"Nombre: "<<nombre<<endl;
+	cout<<"DNI: "<<dni<<endl;
+}
