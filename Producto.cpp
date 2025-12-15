a2ca021ea831f7d729002d4a69e970ec9ecadba7
Samuel Gómez <samusmusic18@gmail.com>
2025-12-15 00:48:56 -0500

    Agregando las funciones de la clase Producto para su uso

---
+++ Producto.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include "Producto.h"
+using namespace std;
+
+Producto::Producto(){
+	id = 0;
+	nombre = " ";
+	stock = 0;
+	precio = 0.0;
+}
+
+Producto::Producto(int i, string n, int s, float p){
+	id = i;
+	nombre = n;
+	stock = s;
+	precio = p;
+}
+
+int Producto::getId(){
+	return id;
+}
+
+string Producto::getNombre(){
+	return nombre;
+}
+
+int Producto::getStock(){
+	return stock;
+}
+
+float Producto::getPrecio(){
+	return precio;
+}
+
+void Producto::setStock(int s){
+	stock = s;
+}
+
+bool Producto::vender(int c){
+	if(c<=stock){
+		stock -= c;
+		return true;
+	}else{
+		return false;
+	}
+}
+
+void Producto::mostrarProducto(){
+	cout<<" ID: "<<id<<" Nombre: "<<nombre<<" Stock: "<<stock<<" Precio: "<<precio<<endl;
+}
