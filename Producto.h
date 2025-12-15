082c43c63fd0f5d9498bdfa0f9c96d79b57c00fd
Samuel Gómez <samusmusic18@gmail.com>
2025-12-15 00:47:43 -0500

    Agregando la clase Producto para la venta

---
+++ Producto.h
@@ -0,0 +1,25 @@
+#ifndef PRODUCTO_H
+#define PRODUCTO_H
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Producto {
+private:
+    int id;
+    string nombre;
+    int stock;
+    float precio;
+public:
+    Producto();
+    Producto(int i, string n, int s, float p);
+    int getId();
+    string getNombre();
+    int getStock();
+    float getPrecio();
+    void setStock(int);
+    bool vender(int);
+    void mostrarProducto();
+};
+
+#endif
